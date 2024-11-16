b1c977640aa6771f22dd368ec09a0edfbed8a398
K Syam Prasad <K Syam Prasad@DESKTOP-69N137D>
2024-11-16 07:39:19 +0530

    Add entity - Product entity

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date createdAt;
+    private String imageFileName;
+}
+
